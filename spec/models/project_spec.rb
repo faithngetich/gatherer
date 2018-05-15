e9ed6cb4db05d5cbc2b151eb4b4fb9d612dea3c8
faithngetich <faithngetich188@gmail.com>
2018-05-15 12:16:29 +0300

    test incomplete task is not done

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -7,5 +7,10 @@ RSpec.describe Project do
     expect(project.done?).to be_truthy 
   end
 
-
+  it "knows that a project with an incomplete task is not done" do
+    project = Project.new
+    task = Task.new
+    project.tasks << task
+    expect(project.done?).to be_falsy
+  end
 end
