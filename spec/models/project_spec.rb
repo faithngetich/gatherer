5327b5ade31222b9f3ff3c40404acd7f79ef03ef
faithngetich <faithngetich188@gmail.com>
2018-05-15 12:50:46 +0300

    mark a project done if tasks are done

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -13,4 +13,10 @@ RSpec.describe Project do
     project.tasks << task
     expect(project.done?).to be_falsy
   end
+
+  it "marks a project done if tasks are done" do
+    project.tasks << task
+    task.mark_completed
+    expect(project).to be_done
+  end
 end
