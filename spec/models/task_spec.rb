875fdf453ca62763414f2bdc0ee35b5d46d12ded
faithngetich <faithngetich188@gmail.com>
2018-05-22 11:15:50 +0300

    another commit

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -13,3 +13,24 @@ RSpec.describe Task do
   end
 
 end
+
+describe "velocity" do
+  let(:task){ Task.new(size:3) }
+
+  it "does not count an incomplete task torward velocity" do
+    expect(task).not_to be_a_part_of_velocity
+    expect(task.points_toward_velocity).to eq(0)
+  end
+
+  it "counts a recently completed task towards velocity" do
+    task.mark_completed(1.day.ago)
+    expect(task).to be_a_part_of_velocity
+    expect(task.points_toward_velocity).to eq(3)
+  end
+
+  it "does not count a long-ago completed task towards velocity" do
+    task.mark_completed(6.months.ago)
+    expect(task).not_to be_a_part_of_velocity
+    expect(task.points_toward_velocity).to eq(0)
+  end
+end
